21c511decfc0f395ad575f87e4f769c4a789f7d7
TumQzy2000 <tompol2015@gmail.com>
2026-01-26 22:41:55 +0700

    00

---
+++ TESTLAB/02.cpp
@@ -0,0 +1,56 @@
+#include "stdio.h"
+#include "conio.h"
+#include "stdlib.h"
+
+void Menu()
+{
+	printf ("***Menu***\n");
+	printf ("Press 1 to show Fibonacci Sequence.\n");
+	printf ("Press 2 t exit.\n");
+}
+
+void Fibonacci()
+{
+	int n,i;
+	int x1 = 0, x2 = 1, Fi;
+	printf ("Enter N : ");
+	scanf ("%d",&n);
+	
+	for(i=1;i<=n;i++)
+	{
+		printf ("%d ",x1);
+		Fi = x1+ x2;
+		x1 = x2;
+		x2 =Fi;
+	}
+	printf ("\n");
+}
+
+main()
+{
+	char choice;
+	while(1)
+	{
+		Menu();
+		choice = getch();
+		printf ("%c\n",choice);
+		
+		if (choice == '1')
+		{
+			Fibonacci();
+		}
+		
+		else if (choice == '2')
+		{
+			printf ("Thank you.\n");
+		}
+		
+		else 
+		{
+			printf ("Wrong choice. Please try again.\n");
+		}
+	getch();
+	system ("cls");
+	}
+	getch();
+}
